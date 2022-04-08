9986ec8d0c7c60a6965a7427fa8af507eb40826e
KALTURA\oleg.sigalov <oleg.sigalov@kaltura.com>
2022-04-08 09:09:11 +0300

    flask try - not working

--- Live.py
+++ Live.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 import MemoryGame, GuessGame, CurrencyRouletteGame, Score
 
 choose_game_output = 'Please choose a game to play:\n1. Memory Game - a sequence of numbers will appear for 1 second and ' \
@@ -9,28 +11,47 @@ game_id = 0
 game_difficulty = 0
 
 def welcome(name):
-    print(f'Hello {name} and welcome to the World of Games (WoG).\nHere you can find many cool games to play.\n')
+    return render_template('welcome.html')
+    # print(f'Hello {name} and welcome to the World of Games (WoG).\nHere you can find many cool games to play.\n')
+    # return
+
+def load_game_web(game_id_=None,game_difficulty_=None):
+    is_won = False
+    print(f'Selected game id: {game_id_} and game difficulty: {game_difficulty_}')
+    if game_id_ == 1:
+        MemoryGame.memory_game_difficulty = game_difficulty_
+        is_won = MemoryGame.play()
+    elif game_id_ == 2:
+        GuessGame.guess_game_difficulty = game_difficulty_
+        is_won = GuessGame.play()
+    elif game_id_ == 3:
+        CurrencyRouletteGame.roulette_game_difficulty = game_difficulty_
+        is_won = CurrencyRouletteGame.play()
+    if is_won:
+        Score.add_score(game_difficulty_)
     return
 
-def load_game():
-    global game_id, game_difficulty
-    is_won = False
-    while True:
-        game_id = get_integer_input(1, 3, choose_game_output)
-        game_difficulty = get_integer_input(1, 5, select_difficulty_output)
-        print(f'Selected game id: {game_id} and game difficulty: {game_difficulty}')
-        if game_id == 1:
-            MemoryGame.memory_game_difficulty = game_difficulty
-            is_won = MemoryGame.play()
-        elif game_id == 2:
-            GuessGame.guess_game_difficulty = game_difficulty
-            is_won = GuessGame.play()
-        elif game_id == 3:
-            CurrencyRouletteGame.roulette_game_difficulty = game_difficulty
-            is_won = CurrencyRouletteGame.play()
-        if is_won:
-            Score.add_score(game_difficulty)
-    return
+# def load_game(game_id_=None,game_difficulty_=None):
+#     global game_id, game_difficulty
+#     is_won = False
+#     while True:
+#         if not game_id_:
+#             game_id = get_integer_input(-1, 3, choose_game_output)
+#         if not game_difficulty_:
+#             game_difficulty = get_integer_input(1, 5, select_difficulty_output)
+#         print(f'Selected game id: {game_id} and game difficulty: {game_difficulty}')
+#         if game_id == 1:
+#             MemoryGame.memory_game_difficulty = game_difficulty
+#             is_won = MemoryGame.play()
+#         elif game_id == 2:
+#             GuessGame.guess_game_difficulty = game_difficulty
+#             is_won = GuessGame.play()
+#         elif game_id == 3:
+#             CurrencyRouletteGame.roulette_game_difficulty = game_difficulty
+#             is_won = CurrencyRouletteGame.play()
+#         if is_won:
+#             Score.add_score(game_difficulty)
+#     return
 
 def get_integer_input(from_number, to_number, message):
     while True:
@@ -43,7 +64,7 @@ def get_integer_input(from_number, to_number, message):
             break
         except SystemExit:
             exit()
-        except:
+        except Exception as e:
             print(f"That's not a valid option! Choose a number between {from_number} and {to_number}\n")
     return number
 
--- MainGame.py
+++ MainGame.py
@@ -1,6 +1,65 @@
-from Live import welcome, load_game
-gamer_name = 'Oleg'
-welcome(gamer_name)
-load_game()
+import sys
+
+from Live import welcome, load_game_web
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('login.html')
+
+@app.route('/start')
+def start():
+    return render_template('welcome.html')
+
+@app.route('/load_game/<game_id>/<game_diff>')
+def load_game(game_id, game_diff):
+    print('Hello world!', file=sys.stdout)
+    return load_game_web(game_id, game_diff)
+
+@app.route('/start_new_game', methods=['GET', 'POST'])
+def start_new_game():
+    if request.method == 'POST':
+        game_id_ = request.form['game_id']
+        game_difficulty_ = request.form['game_diff']
+        return redirect(url_for('load_game', game_id=game_id_,game_diff=game_difficulty_))
+    else:
+        game_id_ = request.args.get('game_id')
+        game_difficulty_ = request.args.get('game_diff')
+        return redirect(url_for('load_game', game_id=game_id_,game_diff=game_difficulty_))
+
+
+
+@app.route('/success/<name>/<last>')
+def success(name, last):
+    return f'welcome {name} {last}'
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user, last='blabla'))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user, last='blabla'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
+
+# gamer_name = 'Oleg'
+# welcome(gamer_name)
+# load_game()
 
 
